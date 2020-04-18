8f7d1ca1b31b091e684cc67c41695bc3cea9a4c9
Vinicius <Vinicius_tessele@hotmail.com>
2020-04-18 16:52:48 -0300

    Primeiro commit

---
+++ MsgWhatsApp/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MsgWhatsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSend = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnSend
+            // 
+            this.BtnSend.Location = new System.Drawing.Point(29, 46);
+            this.BtnSend.Name = "BtnSend";
+            this.BtnSend.Size = new System.Drawing.Size(75, 23);
+            this.BtnSend.TabIndex = 2;
+            this.BtnSend.Text = "Send";
+            this.BtnSend.UseVisualStyleBackColor = true;
+            this.BtnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Email";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 36);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Google Maps";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(193, 90);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 36);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Previsão do tempo";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(193, 132);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 36);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Google Agenda";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BtnSend;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ MsgWhatsApp/Form1.cs
@@ -0,0 +1,260 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Data;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Text;
+using System.Windows.Forms;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.Collections.Generic;
+
+namespace MsgWhatsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+
+            //SMS
+            const string accountSid = "ACb5b821706de321a8e86088e533e103bb";
+            const string authToken = "583238c9e8ae185696e0ddc24c1ff8bd";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "Olá, José.Você confirma sua consulta com o Dr.Pedro no dia 10 / 08 / 2019 as 09:00 ? A Previsão do tempo é de sol, o tempo estimado para chegada no consultório é de 45min Responda SIM ou Não " +
+                "Att Clínica Vida Saúde 44 9 9916 8055",
+                from: new Twilio.Types.PhoneNumber("+12055831869"),
+                to: new Twilio.Types.PhoneNumber("+5544999168055")
+            );
+
+            //Whats
+            //TwilioClient.Init("ACb5b821706de321a8e86088e533e103bb", "583238c9e8ae185696e0ddc24c1ff8bd");
+
+            //var message = MessageResource.Create(
+            //    body: "Olá, José.Você confirma sua consulta com o Dr.Pedro no dia 10 / 08 / 2019 as 09:00 ? A Previsão do tempo é de sol, o tempo estimado para chegada no consultório é de 45min Responda SIM ou Não " +
+            //    "Att Clínica Vida Saúde 44 9 9916 8055";
+            //    from: new Twilio.Types.PhoneNumber("whatsapp:+12055831869"),
+            //    to: new Twilio.Types.PhoneNumber("whatsapp:+14155238886")
+            //);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string senderName = "vinicius";
+            string senderEmail = "vinicius@bla";
+            string message = "conteudo da mensagem";
+
+            System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient();
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Credentials = new System.Net.NetworkCredential("vinitessele@gmail.com", "6419vinikarate");
+            MailMessage mail = new MailMessage();
+            mail.Sender = new System.Net.Mail.MailAddress("vinitessele@gmail.com", "ENVIADOR");
+            mail.From = new MailAddress("vinitessele@gmail.com", "ENVIADOR");
+            mail.To.Add(new MailAddress("vinicius_tessele@hotmail.com", "RECEBEDOR"));
+            mail.Subject = "Contato";
+            mail.Body = " Mensagem do site:<br/> Nome:  " + senderName + "<br/> Email : " + senderEmail + " <br/> Mensagem : " + message;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.High;
+            try
+            {
+                client.Send(mail);
+            }
+            catch (System.Exception erro)
+            {
+                //trata erro
+            }
+            finally
+            {
+                mail = null;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string origin = "Rua Carlos DallAgnolo 121,Toledo, PR";
+            string destination = " R. Santos Dumont, 2546, Toledo, PR";
+            string url = "https://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + origin + "&destinations=" + destination + "&key=AIzaSyCNiXQqjhm3GQ83i2FmXXo835XUOfylz6c";
+            WebRequest request = WebRequest.Create(url);
+            using (WebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {
+                    DataSet dsResult = new DataSet();
+                    dsResult.ReadXml(reader);
+                    string duration = dsResult.Tables["duration"].Rows[0]["text"].ToString();
+                    string distance = dsResult.Tables["distance"].Rows[0]["text"].ToString();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //clima tempo previsao de 15 dias
+            string Url = "http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/6713/days/15?token=96d44d2abf11e7722035ed2714e4d495";
+
+            var request = HttpWebRequest.Create(Url);
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            var data = (JObject)JsonConvert.DeserializeObject(responseString);
+            var date_br = data["data"].Children();
+
+            foreach (var date_br1 in date_br)
+            {
+                var DataPrev = date_br1["date_br"].Value<string>();
+                var chuva1 = date_br1["rain"].Children();
+                var description = date_br1["text_icon"]["text"]["phrase"].Children();
+
+                foreach (var child in chuva1)
+                {
+                    MessageBox.Show(child.ToString());
+                }
+                foreach (var description1 in description)
+                {
+                    MessageBox.Show(description1.ToString());
+                }
+
+
+                Console.WriteLine($"date_br: " + DataPrev + ", reduced: " + description);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string jsonFile = "My First Project-68c8b6a1526c.json";
+            string calanderId = @"vinitessele@gmail.com";
+
+            string[] Scopes = { CalendarService.Scope.Calendar };
+
+            ServiceAccountCredential credential;
+
+            using (var stream =
+                new FileStream(jsonFile, FileMode.Open, FileAccess.Read))
+            {
+                var confg = Google.Apis.Json.NewtonsoftJsonSerializer.Instance.Deserialize<JsonCredentialParameters>(stream);
+                credential = new ServiceAccountCredential(
+                   new ServiceAccountCredential.Initializer(confg.ClientEmail)
+                   {
+                       Scopes = Scopes
+                   }.FromPrivateKey(confg.PrivateKey));
+            }
+
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Calendar API Sample",
+            });
+
+            var calander = service.Calendars.Get(calanderId).Execute();
+            Console.WriteLine("Calander Name :");
+            Console.WriteLine(calander.Summary);
+
+            Console.WriteLine("click for more .. ");
+            Console.Read();
+
+
+            // Define parameters of request.
+            EventsResource.ListRequest listRequest = service.Events.List(calanderId);
+            listRequest.TimeMin = DateTime.Now;
+            listRequest.ShowDeleted = false;
+            listRequest.SingleEvents = true;
+            listRequest.MaxResults = 10;
+            listRequest.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // List events.
+            Events events = listRequest.Execute();
+            Console.WriteLine("Upcoming events:");
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                foreach (var eventItem in events.Items)
+                {
+                    string when = eventItem.Start.DateTime.ToString();
+                    if (String.IsNullOrEmpty(when))
+                    {
+                        when = eventItem.Start.Date;
+                    }
+                    Console.WriteLine("{0} ({1})", eventItem.Summary, when);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No upcoming events found.");
+            }
+            Console.WriteLine("click for more .. ");
+            Console.Read();
+
+            var myevent = DB.Find(x => x.Id == "eventid" + 1);
+
+            var InsertRequest = service.Events.Insert(myevent, calanderId);
+
+            try
+            {
+                InsertRequest.Execute();
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    service.Events.Update(myevent, calanderId, myevent.Id).Execute();
+                    Console.WriteLine("Insert/Update new Event ");
+                    Console.Read();
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("can't Insert/Update new Event ");
+
+                }
+            }
+        }
+
+        static List<Event> DB =
+             new List<Event>() {
+                new Event(){
+                    Id = "eventid" + 1,
+                    Summary = "Consulta Médica Vida Saúde",
+                    Location = "800 Unimed., Cascavel, PR 94103",
+                    Description = "Consulta clínica Vida Saúde.",
+                    Start = new EventDateTime()
+                    {
+                        DateTime = new DateTime(2019, 08, 11, 15, 30, 0),
+                        TimeZone = "America/Los_Angeles",
+                    },
+                    End = new EventDateTime()
+                    {
+                        DateTime = new DateTime(2019, 08, 12, 16, 30, 0),
+                        TimeZone = "America/Los_Angeles",
+                    },
+                     Recurrence = new List<string> { "RRULE:FREQ=DAILY;COUNT=2" },
+                    Attendees = new List<EventAttendee>
+                    {
+                        new EventAttendee() { Email = "julio_cvf@outlook.com"},
+                        new EventAttendee() { Email = "samukantunes@gmail.com"},
+                        new EventAttendee() { Email = "lukaz.17@gmail.com"}
+                    }
+                }
+             };
+    }
+}
+
+
